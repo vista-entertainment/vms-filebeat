94fe1935345533217f3c6052194e88a010a520d6
Gerard Cristofol <Gerard.Cristofol@vista.co>
2019-04-16 17:25:26 +1200

    added systemd configration

--- Deploy.sh
+++ Deploy.sh
@@ -4,3 +4,13 @@ echo "Filebeats install and create serviced entry"
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.0.0-amd64.deb
 sudo dpkg -i filebeat-7.0.0-amd64.deb
 sudo filebeat modules enable haproxy
+
+#TODO create systemd service
+echo "Create systemd config "
+sudo mv systemd/filebeat.service /etc/systemd/system/filebeat.service
+#The unit file, source configuration file or drop-ins of filebeat.service changed on disk
+sudo systemctl daemon-reload
+
+
+sudo systemctl enable filebeat
+sudo systemctl status filebeat
