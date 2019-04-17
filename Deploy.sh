df64398a4119ad0656543bbfc50eb19d54403ec4
Gerard Cristofol <Gerard.Cristofol@vista.co>
2019-04-17 14:19:25 +1200

    Reorder stap to ensure everything is in place when systemd starts

--- Deploy.sh
+++ Deploy.sh
@@ -3,15 +3,13 @@
 echo "Filebeats install and create serviced entry"
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.0.0-amd64.deb
 sudo dpkg -i filebeat-7.0.0-amd64.deb
-sudo filebeat modules enable haproxy
 
 #configure filebeats with propietary fields
-sudo cp etc/haproxy.yml  /etc/filebeat/modules.d/haproxy.yml
-
+sudo cp etc/filebeat.yml  /etc/filebeat/filebeat.yml
 
 #configure module with haproxy file route
 sudo cp etc/haproxy.yml  /etc/filebeat/modules.d/haproxy.yml
-
+sudo filebeat modules enable haproxy
 
 #TODO create systemd service
 echo "Create systemd configuration "
