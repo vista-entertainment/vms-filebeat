26f8c137c285f66cb0bd9bb57feea8379d1a7080
Gerard Cristofol <Gerard.Cristofol@vista.co>
2019-04-17 12:27:22 +1200

    haproxy conf parameters

--- Deploy.sh
+++ Deploy.sh
@@ -5,12 +5,22 @@ curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.0.0-
 sudo dpkg -i filebeat-7.0.0-amd64.deb
 sudo filebeat modules enable haproxy
 
+#configure filebeats with propietary fields
+sudo cp etc/haproxy.yml  /etc/filebeat/modules.d/haproxy.yml
+
+
+#configure module with haproxy file route
+sudo cp etc/haproxy.yml  /etc/filebeat/modules.d/haproxy.yml
+
+
 #TODO create systemd service
-echo "Create systemd config "
-sudo mv systemd/filebeat.service /etc/systemd/system/filebeat.service
+echo "Create systemd configuration "
+sudo cp systemd/filebeat.service /etc/systemd/system/filebeat.service
 #The unit file, source configuration file or drop-ins of filebeat.service changed on disk
 sudo systemctl daemon-reload
 
-
 sudo systemctl enable filebeat
+sudo systemctl status filebeat
+
+#restart filebeat
 sudo systemctl status filebeat
